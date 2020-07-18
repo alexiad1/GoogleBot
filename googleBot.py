31ab3228b663213e4f928fcafe76b57f2a24c116
Alexis Diaz <alexisdiaz@Alexiss-MacBook-Pro-2.local>
2020-07-17 22:12:22 -0700

    created the bot and added first google capabilities

--- googleBot.py
+++ googleBot.py
@@ -1,1 +1,14 @@
-import discord.py
+import discord
+from googlesearch import search
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='^')
+
+@client.command(name='Search', description='Google searches and sends the top results to text channel, default # of results are one but can be changed in request')
+async def LetMeGoogleThat(ctx, query, results=1):
+    for i in search(query, num=results, stop=results, verify_ssl=False):
+        await ctx.send(i)
+
+
+token = 'NzMzNjQzNDM3NzM1NDExNzgz.XxGKlg.tPYPYgXCR6QC6DFIxPREWELFmz0'
+client.run(token)
