b06e3b7eb4a2563b39bb29ddbd29211ea3bc5807
Alexis Diaz <alexisdiaz@Alexiss-MacBook-Pro-2.local>
2020-07-18 21:33:40 -0700

    adjusted search func

--- googleBot.py
+++ googleBot.py
@@ -5,8 +5,14 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='^')
 
 @client.command(name='Search', description='Google searches and sends the top results to text channel, default # of results are one but can be changed in request')
-async def LetMeGoogleThat(ctx, query, results=1):
-    for i in search(query, num=results, stop=results, verify_ssl=False):
+async def LetMeGoogleThat(ctx, results=1):
+    await ctx.send('Please enter search Query')
+
+    query = await client.wait_for('message')
+
+    await ctx.send(f'now searching for {results} results for "{query.content}"')
+
+    for i in search(str(query.content), num=results+1, stop=results+1, verify_ssl=False):
         await ctx.send(i)
 
 
